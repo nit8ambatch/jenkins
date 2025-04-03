73fe37e5df3ccf46809ecdeae1a57b175943cabb
likhi <likhi@gmail.com>
2025-04-03 15:11:34 +0530

    feat: new file created

--- swiggy.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-yum install httpd git -y
-systemctl start httpd
-systemctl status httpd
-cd /var/www/html
-git clone https://github.com/karishma1521success/swiggy-clone.git
-mv swiggy-clone/* .
-tail -100f /var/log/httpd/access_log
